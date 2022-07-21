5f345bb855f96cf2efcb0e1cf06b933e408f622c
Maks Michalski <mamich1998@gmail.com>
2022-07-21 08:55:48 +0200

    Ukończenie skorupy logowania oraz porpawa responsywności

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './presentation/nav/nav.component';
+import { LoginComponent } from './presentation/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
